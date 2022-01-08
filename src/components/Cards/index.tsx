eed8dd046ed2f05fb04747cc92848442c31a1ebf
Jose Romary <joseromarybrandao@gmail.com>
2022-01-08 01:45:55 -0300

    style home finalizada + save index-card

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -12,21 +12,31 @@ export function Cards() {
             <span>Create Sign in Validation</span>
           </div>
           <div className="moreOptions">
-            <img src={moreoptions} alt="to do list logo" height={30} width={30} />
+            <img
+              src={moreoptions}
+              alt="to do list logo"
+              height={30}
+              width={30}
+            />
           </div>
         </div>
 
         <div className="description">
           <span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
           </span>
         </div>
 
         <div className="footer">
-          <span>
-          Created March 30, 2021 1:24pm
-          </span>
-
+          <span>Created March 30, 2021 1:24pm</span>
         </div>
       </Content>
     </Container>
